28705b6a25a9a76de49194b47837e477a9fd53c7
ashe09 <creation.oasis21la@gmail.com>
2023-11-15 20:38:16 +0900

    コメントいただいた部分の修正を反映

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,17 +2,11 @@
 require "date"
 require "optparse"
 
-# ライブラリDate 本日を取得
 today = Date.today
-
-# ライブラリoptparse コマンドラインのオプション
 params = ARGV.getopts("m:y:")
-input_month = params["m"] ? params["m"] : today.mon
-input_year = params["y"] ? params["y"] : today.year
-month = input_month.to_i
-year = input_year.to_i
+month = params["m"] ? params["m"].to_i : today.mon
+year = params["y"] ? params["y"].to_i : today.year
 
-# 引数の数値範囲外の場合にエラーメッセージを表示し、処理を終了
 unless month.between?(1, 12)
   puts "cal: 13 is neither a month number (1..12) nor a name"
   return
@@ -23,36 +17,28 @@ unless year.between?(1, 9999)
   return
 end
 
-# 月初めの日・最後の日・月初めの日の曜日を取得
 first_day = Date.new(year, month, 1)
 last_day = Date.new(year, month, -1)
 beginning_of_month = first_day.wday
 
-# カレンダーの月・年・曜日の表示
 puts ("#{month}月 #{year}").center(20)
 puts "日 月 火 水 木 金 土"
 
-# 月初めの日の曜日まで空白を入れる
-i = 0
-while i < beginning_of_month
-  space = ""
-  printf("%3s", space)
-  i += 1
-end
+space = format("%3s", "")
+print space * beginning_of_month
 
-# 日付を表示
-(first_day..last_day).each {|x|
-  if x == today
+(first_day..last_day).each do |day|
+  if day == today
     print "\e[30m\e[47m"
-    printf("%2s", x.day.to_s)
+    printf("%2s", day.day.to_s)
     print "\e[0m"
     print " "
   else
-    printf("%2s", x.day.to_s)
+    printf("%2s", day.day.to_s)
     print " "
   end
-  if x.wday == 6
+  if day.saturday?
     print "\n"
   end
-}
+end
 puts "\n"
