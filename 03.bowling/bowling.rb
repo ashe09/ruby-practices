bb62c62793d2180d5352cc8b3f8c9e4e003199e5
ashe09 <creation.oasis21la@gmail.com>
2023-11-26 21:14:54 +0900

    マジックナンバーを定数へ変更し、スコア計算部分を元に戻すよう修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,9 +4,13 @@
 input_score = ARGV[0]
 scores = input_score.split(',')
 shots = []
+
+STRIKE = 'X'
+STRIKE_SCORE = 10
+
 scores.each do |score|
-  if score == 'X'
-    shots << 10
+  if score == STRIKE
+    shots << STRIKE_SCORE
     shots << 0
   else
     shots << score.to_i
@@ -14,12 +18,18 @@ scores.each do |score|
 end
 
 frames = shots.each_slice(2).to_a
+frame_number = 10
 point = 0
-10.times do |i|
-  point += if frames[i][0] == 10
-             frames[i + 1].sum + frames[i + 2][0]
-           elsif frames[i].sum == 10
-             frames[i + 1][0]
+
+frame_number.times do |i|
+  point += if frames[i][0] == STRIKE_SCORE
+             if frames[i + 1][0] == STRIKE_SCORE
+               frames[i][0] + frames[i + 1].sum + frames[i + 2][0]
+             else
+               frames[i][0] + frames[i + 1].sum
+             end
+           elsif frames[i].sum == STRIKE_SCORE
+             frames[i].sum + frames[i + 1][0]
            else
              frames[i].sum
            end
